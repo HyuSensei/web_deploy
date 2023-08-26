7a421bad22eb7496f51a65cad33bcd65546236c1
HyuSensei <110617220+HyuSensei@users.noreply.github.com>
2023-08-26 12:23:14 +0700

    Update vnpay_return.php

--- vnpay_return.php
+++ vnpay_return.php
@@ -44,7 +44,7 @@
     $vnp_TmnCode = "CGXZLS0Z"; //Mã định danh merchant kết nối (Terminal Id)
     $vnp_HashSecret = "XNBCJFAKAZQSGTARRLGCHVZWCIOIGSHN"; //Secret key
     $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    $vnp_Returnurl = "http://localhost/do_an_web-main/vnpay_return.php";
+    $vnp_Returnurl = "https://skinlele.azurewebsites.net/vnpay_return.php";
     $vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
     $apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
     //Config input format
