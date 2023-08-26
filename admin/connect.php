13d2afae71c971ac0c587891687197d6faff1653
HyuSensei <110617220+HyuSensei@users.noreply.github.com>
2023-08-26 12:10:07 +0700

    Update connect.php

--- admin/connect.php
+++ admin/connect.php
@@ -5,5 +5,5 @@
 // mysqli_set_charset($connect, 'utf8');
 $connect = mysqli_init();
 mysqli_ssl_set($connect, NULL, NULL, "../db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connect, "skinlele-database.mysql.database.azure.com", "skinlele", "huyphan2002@", "do_an_web", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($connect, "skinlele-database.mysql.database.azure.com", "skinlele", "huyphan2002@", "web_deploy", 3306, MYSQLI_CLIENT_SSL);
 mysqli_set_charset($connect, 'utf8');
