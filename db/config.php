7cc87a5735c409af489e0f725769f2adeb019bba
HyuSensei <110617220+HyuSensei@users.noreply.github.com>
2023-08-26 12:09:46 +0700

    Update config.php

--- db/config.php
+++ db/config.php
@@ -15,7 +15,7 @@
 
 $connect = mysqli_init();
 mysqli_ssl_set($connect, NULL, NULL, "db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($connect, "skinlele-database.mysql.database.azure.com", "skinlele", "huyphan2002@", "do_an_web", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($connect, "skinlele-database.mysql.database.azure.com", "skinlele", "huyphan2002@", "web_deploy", 3306, MYSQLI_CLIENT_SSL);
 mysqli_set_charset($connect, 'utf8');
 if ($connect === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
